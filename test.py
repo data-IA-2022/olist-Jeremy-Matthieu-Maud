d3e24b4932b62cebb0c6f0d7abcb200f3762afef
Jeremy MIKALEFF <jeremy.mikaleff@gmail.com>
2023-03-29 11:45:37 +0200

    sync 6

---
+++ test.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+# Connexion à la base de données PostgreSQL
+conn = psycopg2.connect(
+    host="localhost",
+    database="postgres",
+    user="postgres",
+    password="greta2023"
+)
+
+# Création d'un curseur pour exécuter des requêtes SQL
+cursor = conn.cursor()
+
+# Exemple : Exécuter une requête SQL pour afficher le contenu de la table "mytable"
+cursor.execute("SELECT * FROM olist_customers_dataset")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+# Fermeture de la connexion à la base de données PostgreSQL
+conn.close()
