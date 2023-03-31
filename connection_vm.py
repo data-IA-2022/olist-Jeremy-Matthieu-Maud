c7ea65347b359d1afbd1a237050a3d430eb79f6a
Jeremy MIKALEFF <jeremy.mikaleff@gmail.com>
2023-03-31 15:01:30 +0200

    sync 13

--- connection_vm.py
+++ connection_vm.py
@@ -14,7 +14,7 @@ ssh.connect(hostname='gretap2-mjm.francecentral.cloudapp.azure.com', username='g
 
 files=['olist_customers_dataset', 'olist_geolocation_dataset', 'olist_order_items_dataset', 
        'olist_order_payments_dataset', 'olist_orders_dataset', 'olist_products_dataset', 
-       'olist_sellers_dataset', 'product_category_name_translation', 'olist_order_reviews_dataset'] #'olist_order_reviews_dataset',
+       'olist_sellers_dataset', 'olist_order_reviews_dataset']
 
 # Paramètres de connexion à la base de données distance
 host_dist = 'localhost'
