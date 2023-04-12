7ec21fce24eac31a192b7861398c9e112e163c71
Matthieu Ernoux <matt.er41@hotmail.fr>
2023-04-12 11:00:47 +0200

    ajout sur la base et affichage

--- olist/app/views.py
+++ olist/app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 import pandas as pd
 from .models import product_category_name_translation
 from .forms import TraductionForm
+from django.views.decorators.csrf import csrf_exempt
 
 def ajouter_traduction(request):
     if request.method == 'POST':
@@ -22,13 +23,28 @@ def home(request):
     data_dict = data.to_dict('records')
     return render(request, 'home.html', {'data': data_dict})
 
-def test(request):
-    donnees = product_category_name_translation.objects.all()
-    return render(request, 'test.html', {'donnees': donnees})
 
-"""def afficher_tout(request):
-    donnees = product_category_name_translation.objects.all()
-    context = {
-        'donnees': donnees
-    }
-    return render(request, 'test.html', context)"""
+
+@csrf_exempt
+
+def test(request):
+    afficher = False
+    if request.method == 'POST':
+        form = TraductionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('test')
+    else:
+        form = TraductionForm()
+
+    donnees = []
+    afficher = False
+    if 'afficher' in request.POST:
+        donnees = product_category_name_translation.objects.all()
+        afficher = True
+    elif 'masquer' in request.POST:
+        afficher = False
+
+    context = {'form': form, 'donnees': donnees, 'afficher': afficher}
+    return render(request, 'test.html', context)
+
